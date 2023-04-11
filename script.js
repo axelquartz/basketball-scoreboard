71c6c94173b8022bb56e27e40f476d8681b3d924
Axel Robles <axelarep@gmail.com>
2023-04-11 12:40:23 -0600

    Add complete scoreboard and points buttons functionality

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//Points
+let homePoints = 0;
+let guestPoints = 0;
+
 // Display elements DOM
-let homeScore = document.getElementById('home-score')
-let guestScore = document.getElementById('guest-score')
+let homeScoreboard = document.getElementById('home-scoreboard')
+let guestScoreboard = document.getElementById('guest-scoreboard')
 
 // Action buttons DOM
 // Home buttons
@@ -12,3 +16,35 @@ let home3pt = document.getElementById('home-3pt')
 let guest1pt = document.getElementById('guest-1pt')
 let guest2pt = document.getElementById('guest-2pt')
 let guest3pt = document.getElementById('guest-3pt')
+
+//Buttons interactions - Home player
+home1pt.addEventListener('click', () => {
+    homePoints += 1;
+    homeScoreboard.innerHTML = '<h2>' + homePoints + '</h2>';
+})
+
+home2pt.addEventListener('click', () => {
+    homePoints += 2;
+    homeScoreboard.innerHTML = '<h2>' + homePoints + '</h2>';
+})
+
+home3pt.addEventListener('click', () => {
+    homePoints += 3;
+    homeScoreboard.innerHTML = '<h2>' + homePoints + '</h2>';
+})
+
+//Buttons interactions - Guest player
+guest1pt.addEventListener('click', () => {
+    guestPoints += 1;
+    guestScoreboard.innerHTML = '<h2>' + guestPoints + '</h2>';
+})
+
+guest2pt.addEventListener('click', () => {
+    guestPoints += 2;
+    guestScoreboard.innerHTML = '<h2>' + guestPoints + '</h2>';
+})
+
+guest3pt.addEventListener('click', () => {
+    guestPoints += 3;
+    guestScoreboard.innerHTML = '<h2>' + guestPoints + '</h2>';
+})
