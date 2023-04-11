6052178e7a486ba38009459235707dc11b905f4e
Axel Robles <axelarep@gmail.com>
2023-04-11 12:01:09 -0600

    Add DOM interactive variables

---
+++ script.js
@@ -0,0 +1,14 @@
+// Display elements DOM
+let homeScore = document.getElementById('home-score')
+let guestScore = document.getElementById('guest-score')
+
+// Action buttons DOM
+// Home buttons
+let home1pt = document.getElementById('home-1pt')
+let home2pt = document.getElementById('home-2pt')
+let home3pt = document.getElementById('home-3pt')
+
+// Guest buttons
+let guest1pt = document.getElementById('guest-1pt')
+let guest2pt = document.getElementById('guest-2pt')
+let guest3pt = document.getElementById('guest-3pt')
